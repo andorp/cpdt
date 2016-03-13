81114e6ce68738ad42b8d8208d51f2d27e8a2317
Andor Penzes <andor.penzes@gmail.com>
2016-03-14 00:29:48 +0100

    Chapter 3.6 - 3.7.

--- chapter03.v
+++ chapter03.v
@@ -323,3 +323,154 @@ Theorem elength_eapp : forall el1 el2 : even_list ,
                     olength (oapp ol el) = plus (olength ol) (elength el))); crush.
 Qed.
 
+(* 3.6 Reflexive Types *)
+
+(*
+A kind of inductive type called a reflexive type includes at least
+one constructor that takes as an argument a function returning the
+same tye we are defining.
+*)
+
+Inductive pformula : Set :=
+| Truth : pformula
+| Falsehood : pformula
+| Conjuction : pformula -> pformula -> pformula
+.
+
+Fixpoint pformulaDenote (f : pformula) : Prop :=
+  match f with
+  | Truth => True
+  | Falsehood => False
+  | Conjuction f1 f2 => pformulaDenote f1 /\ pformulaDenote f2
+  end
+.
+
+Inductive formula : Set :=
+| Eq : nat -> nat -> formula
+| And : formula -> formula -> formula
+| Forall : (nat -> formula) -> formula (* Constructor which makes this type reflexive *)
+.
+
+Example forall_refl : formula := Forall (fun x =>  Eq x x).
+
+Fixpoint formulaDenote (f : formula) : Prop :=
+  match f with
+  | Eq n1 n2 => n1 = n2
+  | And f1 f2 => formulaDenote f1 /\ formulaDenote f2
+  | Forall f' => forall n : nat, formulaDenote (f' n)
+  end
+.
+
+Fixpoint swapper (f : formula) : formula :=
+  match f with
+  | Eq n1 n2 => Eq n2 n1
+  | And f1 f2 => And (swapper f2) (swapper f1)
+  | Forall f' => Forall (fun n => swapper (f' n))
+  end
+.
+
+Theorem swapper_preserves_truth : forall f , formulaDenote f -> formulaDenote (swapper f).
+   induction f; crush.
+Qed.
+
+Check formula_ind.
+
+(*
+Inductive term : Set :=
+| App : term -> term -> term
+| Abs : (term -> term) -> term
+.
+Error: Non strictly positive occurrence of "term" in "(term -> term) -> term".
+
+Our candidate definition above violates the positivity requirement because it
+involves an argument of type 'term -> term', where the type term that we are
+defining appears to the left of an arrow. The candidate type of App is fine,
+however, since every occurrence of term is either a consturctor argument or
+the final result type.
+*)
+
+(* 3.7 An Interlude on Induction Principles *)
+
+Print nat_ind.
+
+Check nat_rect.
+
+Print nat_rec.
+
+Fixpoint plus_recursive (n : nat) : nat -> nat :=
+  match n with
+  | O => fun m => m
+  | S n' => fun m => S (plus_recursive n' m)
+  end
+.
+
+Definition plus_rec : nat -> nat -> nat :=
+  nat_rec (fun _ : nat => nat -> nat) (fun m => m) (fun _ r m => S (r m))
+.
+
+Extraction "plus.hs" plus_rec.
+
+Theorem plus_equivalent :
+  plus_recursive = plus_rec.
+    reflexivity.
+Qed.
+
+Print nat_rect.
+
+Fixpoint nat_rect' (P : nat -> Type)
+         (HO : P O)
+         (HS : forall n , P n -> P (S n)) (n : nat) :=
+  match n return P n with
+  | O => HO
+  | S n' => HS n' (nat_rect' P HO HS n')
+  end
+.
+
+Section nat_ind'.
+  Variable P : nat -> Prop.
+  Hypothesis O_case : P O.
+  Hypothesis S_case : forall n : nat , P n -> P (S n).
+  Fixpoint nat_ind' (n : nat) : P n :=
+    match n with
+    | O => O_case
+    | S n' => S_case (nat_ind' n')
+    end
+  .
+End nat_ind'.
+
+Print even_list_mut.
+
+Section even_list_mut'.
+  Variable Peven : even_list -> Prop.
+  Variable Podd : odd_list -> Prop.
+  Hypothesis ENil_case : Peven ENil.
+  Hypothesis ECons_case : forall (n : nat) (o : odd_list) , Podd o -> Peven (ECons n o).
+  Hypothesis OCons_case : forall (n : nat) (e : even_list) , Peven e -> Podd (OCons n e).
+  Fixpoint even_list_mut' (e : even_list) : Peven e :=
+    match e with
+    | ENil => ENil_case
+    | ECons n o => ECons_case n (odd_list_mut' o)
+    end
+  with
+  odd_list_mut' (o : odd_list) : Podd o :=
+    match o with
+    | OCons n e => OCons_case n (even_list_mut' e)
+    end.
+End even_list_mut'.
+
+Section formula_ind'.
+  Variable P : formula -> Prop.
+  Hypothesis Eq_case : forall n1 n2 : nat , P (Eq n1 n2).
+  Hypothesis And_case : forall f1 f2 : formula,
+      P f1 -> P f2 -> P (And f1 f2).
+  Hypothesis Forall_case : forall f : nat -> formula ,
+      (forall n : nat , P (f n)) -> P (Forall f).
+  Fixpoint formula_ind' (f : formula) : P f :=
+    match f with
+    | Eq n1 n2 => Eq_case n1 n2
+    | And f1 f2 => And_case (formula_ind' f1) (formula_ind' f2)
+    | Forall f' => Forall_case f' (fun n => formula_ind' (f' n))
+    end
+  .
+End formula_ind'.
+
---
+++ plus.hs
@@ -0,0 +1,22 @@
+module Plus where
+
+import qualified Prelude
+
+data Nat =
+   O
+ | S Nat
+
+nat_rect :: a1 -> (Nat -> a1 -> a1) -> Nat -> a1
+nat_rect f f0 n =
+  case n of {
+   O -> f;
+   S n0 -> f0 n0 (nat_rect f f0 n0)}
+
+nat_rec :: a1 -> (Nat -> a1 -> a1) -> Nat -> a1
+nat_rec =
+  nat_rect
+
+plus_rec :: Nat -> Nat -> Nat
+plus_rec =
+  nat_rec (\m -> m) (\n r m -> S (r m))
+
